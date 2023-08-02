bc2d9fbfcda4c15fcf85354dd212f9153c4c51e4
Zelimkhan-P <chuvokhi@gmail.com>
2023-08-02 21:12:43 +0300

    Redux Toolkit Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
 
-  return <div className="App">App</div>;
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = async () => {
+    try {
+      const res = await fetchDataFromApi("/movie/popular");
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    } catch (error) {
+      console.error("Error fetching data from API:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
